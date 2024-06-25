89f15b32943b963aae436c1f2c3e9ed4eb278789
mahmoudkora <mahmoyd.kora40@gmail.com>
2024-06-25 15:11:01 +0300

    commint

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -22,14 +22,14 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app['config']['arb.mode'] = Setting::where('key', 'ARB_MODE')->first()->value;
-        $this->app['config']['arb.redirect.success'] = Setting::where('key', 'ARB_REDIRECT_SUCCESS')->first()->value;
-        $this->app['config']['arb.redirect.fail'] = Setting::where('key', 'ARB_REDIRECT_FAIL')->first()->value;
+        // $this->app['config']['arb.mode'] = Setting::where('key', 'ARB_MODE')->first()->value;
+        // $this->app['config']['arb.redirect.success'] = Setting::where('key', 'ARB_REDIRECT_SUCCESS')->first()->value;
+        // $this->app['config']['arb.redirect.fail'] = Setting::where('key', 'ARB_REDIRECT_FAIL')->first()->value;
 
-        $this->app['config']['sms.SMS_API_KEY'] = Setting::where('key', 'SMS_API_KEY')->first()->value;
-        $this->app['config']['sms.SMS_USER_SENDER'] = Setting::where('key', 'SMS_USER_SENDER')->first()->value;
-        $this->app['config']['sms.SMS_USER_NAME'] = Setting::where('key', 'SMS_USER_NAME')->first()->value;
-        $this->app['config']['sms.SMS_SEND_URL'] = Setting::where('key', 'SMS_SEND_URL')->first()->value;
+        // $this->app['config']['sms.SMS_API_KEY'] = Setting::where('key', 'SMS_API_KEY')->first()->value;
+        // $this->app['config']['sms.SMS_USER_SENDER'] = Setting::where('key', 'SMS_USER_SENDER')->first()->value;
+        // $this->app['config']['sms.SMS_USER_NAME'] = Setting::where('key', 'SMS_USER_NAME')->first()->value;
+        // $this->app['config']['sms.SMS_SEND_URL'] = Setting::where('key', 'SMS_SEND_URL')->first()->value;
 
 
     }
